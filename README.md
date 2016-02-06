6269af05f8cc8b13a0e1c2b4fab2f66ecc58ce53
Max Millien <maxmillien@gmail.com>
2016-02-06 12:35:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# rain-util-fs #
+
+[![Build Status](https://travis-ci.org/maxmill/rain-util-fs.svg?style=flat-square)](https://travis-ci.org/maxmill/rain-util-fs)
+[![npm](https://img.shields.io/npm/v/rain-util-fs.svg?style=flat-square)]()
+[![npm](https://img.shields.io/npm/dt/rain-util-fs.svg)]()
+
+
+Simplistic generator based toolkit for consuming apis, accessing postgres, and managing filesystem
+
+```
+npm i rain-util-fs
+var $util = require('rain-util-fs');
+```
+
+extends co-fs - all core node fs methods available as generators
+```
+$util.fs.download   - (file or file array) downloads url(s) to file(s)
+$util.fs.upsert     - (dir or dir array) creates dir if non-existent (uses mkdirp)
+$util.fs.fetch     - (file or dir) read file or directory contents
+$util.fs.rimraf     - (path) yieldable rm -rf
+$util.fs.objectify          - (path) converts directory and contents into node module
+$util.fs.json.(read|write)  - (file, obj, options)
+```
+
+
+### credits ###
+
+- https://github.com/jprichardson/node-jsonfile
+- https://github.com/substack/node-mkdirp
+- https://www.npmjs.com/package/array-generators
---
+++ test/index.js
@@ -0,0 +1,51 @@
+const test = require('tape-catch');
+const path = require('path');
+const $fs = require('../bin');
+const coTape = require('co-tape');
+
+
+test('upsert recusively', coTape(function* (t) {
+    yield $fs.rimraf(path.resolve('./data'));
+    console.log('creating directory: ' + (yield $fs.upsert(['./data/1/2/3', './data/3/2/1'])));
+
+    var passed = (yield $fs.exists('./data/1'));
+    t[passed === true ? 'pass' : 'fail']('upsert recusively');
+
+    t.end();
+}));
+
+//
+test('fetch file', coTape(function* (t) {
+    var fetched = yield $fs.fetch(path.resolve('./LICENSE'));
+    var passed = (fetched.length && fetched.length > 0);
+
+    t[passed === true ? 'pass' : 'fail']('upsert recusively');
+
+    t.end();
+}));
+
+test('fetch path', coTape(function* (t) {
+    var fetched = yield $fs.fetch(path.resolve('./'));
+    var passed = (fetched.length && fetched.length > 0);
+
+    t[passed === true ? 'pass' : 'fail']('upsert recusively');
+
+    t.end();
+}));
+
+
+test('file download', coTape(function* (t) {
+    var file = {
+        url: 'https://joyeur.files.wordpress.com/2011/07/nodejs.png',
+        src: (path.resolve('./data') + '/img.png')
+    };
+
+    var filePath = (yield $fs.download(file));
+    console.log(filePath);
+
+    var passed = (yield $fs.exists(file.src));
+    t[passed === true ? 'pass' : 'fail']('file download');
+
+    t.end();
+}));
+
